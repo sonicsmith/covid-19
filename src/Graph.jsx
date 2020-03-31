98442dad261c1d63d5d78d8671b4ed448bba55ac
sonicsmith <thesonicsmith@gmail.com>
2020-03-31 21:21:10 +1300

    Using React-Select for the selection component. Added Deaths

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState, useMemo } from "react"
 import "./App.css"
 import { Line } from "react-chartjs-2"
-import { Box, Select, RadioButtonGroup } from "grommet"
+import { Box, RadioButtonGroup } from "grommet"
+import Select from "react-select"
 import useLiveData from "./useLiveData"
 import useCountryPopulation from "./useCountryPopulation"
 
 const colors = [
-  "rgba(111, 255, 176, 0.7)",
-  "rgba(253, 111, 255, 0.7)",
-  "rgba(129, 252, 237, 0.7)",
-  "rgba(255, 202, 88, 0.7)"
+  "rgba(111, 255, 176, 0.8)",
+  "rgba(253, 111, 255, 0.8)",
+  "rgba(111, 255, 176, 0.4)",
+  "rgba(253, 111, 255, 0.4)"
+  // "rgba(129, 252, 237, 0.7)",
+  // "rgba(255, 202, 88, 0.7)"
 ]
 
 const getAsPopulationPercentage = (number, population) => {
-  return (number * 100) / population
+  if (population) {
+    return (number * 100) / population
+  }
+  return number
 }
 
 const getValuesAsLog = values => {
@@ -22,9 +28,20 @@ const getValuesAsLog = values => {
   })
 }
 
+const getCumulative = updatedCases => {
+  return updatedCases.map((c, i) => {
+    if (i > 1) {
+      return c - updatedCases[i - 1]
+    } else {
+      return c
+    }
+  })
+}
+
 const Graph = () => {
   const liveData = useLiveData()
   const [cases, setCases] = useState({})
+  const [deaths, setDeaths] = useState({})
   const [days, setDays] = useState({})
   const [selectedCountries, setSelectedCountries] = useState(["Italy", "US"])
   const [isCumulativeGraph, setIsCumulativeGraph] = useState(true)
@@ -41,43 +58,47 @@ const Graph = () => {
   useEffect(() => {
     if (liveData[selectedCountries[0]]) {
       const updatedCases = {}
+      const updatedDeaths = {}
       const updatesDates = {}
+
       selectedCountries.forEach((selectedCountry, i) => {
         const allCases = liveData[selectedCountry].map(o => {
           const activeCases = Math.max(o.confirmed - o.recovered, 0)
-          if (isPopulationPercentage) {
-            if (!countryPopulation[i]) {
-              alert(
-                `Couldn't get population for ${selectedCountry}. Please select raw numbers`
-              )
-              return 0
-            }
-            return getAsPopulationPercentage(activeCases, countryPopulation[i])
-          } else {
-            return activeCases
-          }
+          return getAsPopulationPercentage(
+            activeCases,
+            isPopulationPercentage && countryPopulation[i]
+          )
+        })
+        const allDeaths = liveData[selectedCountry].map(o => {
+          return getAsPopulationPercentage(
+            o.deaths,
+            isPopulationPercentage && countryPopulation[i]
+          )
         })
         const dates = liveData[selectedCountry].map(o => o.date)
         const firstConfirmed = allCases.findIndex(c => c > 0)
         const casesSinceFirst = allCases.slice(firstConfirmed)
-        const logCases = getValuesAsLog(casesSinceFirst)
-        updatedCases[selectedCountry] = isLogMode ? logCases : casesSinceFirst
+        const deathsSinceFirst = allDeaths.slice(firstConfirmed)
+        updatedCases[selectedCountry] = isLogMode
+          ? getValuesAsLog(casesSinceFirst)
+          : casesSinceFirst
+        updatedDeaths[selectedCountry] = isLogMode
+          ? getValuesAsLog(deathsSinceFirst)
+          : deathsSinceFirst
         updatesDates[selectedCountry] = dates
           .slice(firstConfirmed)
           .map((d, i) => i)
         // If we want daily values
         if (!isCumulativeGraph) {
-          const dailyValues = updatedCases[selectedCountry].map((c, i) => {
-            if (i > 1) {
-              return c - updatedCases[selectedCountry][i - 1]
-            } else {
-              return c
-            }
-          })
-          updatedCases[selectedCountry] = dailyValues
+          const dailyCases = getCumulative(updatedCases[selectedCountry])
+          const dailyDeaths = getCumulative(updatedDeaths[selectedCountry])
+          updatedCases[selectedCountry] = dailyCases
+          updatedDeaths[selectedCountry] = dailyDeaths
+          console.log("Deaths", dailyDeaths.length)
         }
       })
       setCases(updatedCases)
+      setDeaths(updatedDeaths)
       setDays(updatesDates)
     }
   }, [
@@ -90,9 +111,17 @@ const Graph = () => {
 
   const graphData = useMemo(() => {
     if (Object.keys(cases).length) {
-      const datasets = Object.values(cases)
-      return datasets.map((dataset, i) => ({
-        label: `${Object.keys(cases)[i]}`,
+      const casesDatasets = Object.values(cases)
+      const deathsDatasets = Object.values(deaths)
+      const allDatasets = [...casesDatasets, ...deathsDatasets]
+      const labels = [
+        `${Object.keys(cases)[0]} Infected`,
+        `${Object.keys(cases)[1]} Infected`,
+        `${Object.keys(cases)[0]} Deaths`,
+        `${Object.keys(cases)[1]} Deaths`
+      ]
+      return allDatasets.map((dataset, i) => ({
+        label: `${labels[i]}`,
         borderColor: colors[i],
         pointStyle: "line",
         data: dataset
@@ -162,22 +191,26 @@ const Graph = () => {
         />
       </Box>
       Countries to Compare:
-      <Box direction="column" pad="medium" align="center">
-        {selectedCountries.map((selected, i) => {
-          return (
+      {/* <Box direction="column" pad="medium" align="center"> */}
+      {selectedCountries.map((selected, i) => {
+        return (
+          <div key={`div${i}`} style={{ color: "black" }}>
             <Select
-              key={`country${i}`}
-              options={countries}
-              value={selected}
-              onChange={({ option }) => {
+              key={`select${i}`}
+              options={countries.map(c => {
+                return { value: c, label: c }
+              })}
+              value={{ value: selected, label: selected }}
+              onChange={({ value }) => {
                 const newSelectedCountries = [...selectedCountries]
-                newSelectedCountries[i] = option
+                newSelectedCountries[i] = value
                 setSelectedCountries(newSelectedCountries)
               }}
             />
-          )
-        })}
-      </Box>
+          </div>
+        )
+      })}
+      {/* </Box> */}
       Graph Values:
       <Box direction="column" pad="medium">
         <RadioButtonGroup
